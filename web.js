87fdc1fb9c835e8e99ad8b2365e6ded8bd7fe814
Nikhil Nanjappa <nnanjappa@Nikhils-MacBook-Pro.local>
2016-04-08 17:50:43 +0530

    edited server code

--- web.js
+++ web.js
@@ -1,7 +1,14 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get(/^(.+)$/, function (req, res) {
+    res.sendFile(__dirname + req.params[0]);
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
