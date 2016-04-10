84e74c11ecae6d92796f3d098cc550e81f7e3326
Nikhil Nanjappa <nnanjappa@Nikhils-MacBook-Pro.local>
2016-04-10 17:30:48 +0530

    edited

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var compression = require('compression');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
